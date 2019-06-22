c05ff8226e5e015a815ef9dbed17f6401a113c16
StrikerOne65 <golamrabbani20014@gmail.com>
2019-06-22 14:54:28 +0530

    Added scroll to nav and foot

--- resourses/js/custom.js
+++ resourses/js/custom.js
@@ -14,5 +14,20 @@ $(document).ready(function(){
         $("nav").removeClass("sticky-nav");  
       }
     });
+    /* Scroll Nav*/
+        $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
     
 });
