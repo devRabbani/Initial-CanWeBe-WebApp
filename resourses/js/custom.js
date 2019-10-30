5333a78568480e43d13d9c00ef7a72e014d22ca9
StrikerOne65 <golamrabbani20014@gmail.com>
2019-10-30 22:52:00 +0530

    Fixing video background

--- resourses/js/custom.js
+++ resourses/js/custom.js
@@ -1,50 +1,49 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $(".owl-carousel").owlCarousel({
-      items:1,
-      autoplay:true,
-      smartSpeed:700,
-      loop:true,
-      autoplayHoverPause:true
+    items: 1,
+    autoplay: true,
+    smartSpeed: 700,
+    loop: true,
+    autoplayHoverPause: true
   });
-    
-     $(".hero-text-box").waypoint(function(direction){
-     if  (direction == "down") {
-       $("nav").addClass("sticky-nav");
-      }else{
-        $("nav").removeClass("sticky-nav");  
+
+  $(".hero-text-box").waypoint(function (direction) {
+    if (direction == "down") {
+      $("nav").addClass("sticky-nav");
+    } else {
+      $("nav").removeClass("sticky-nav");
+    }
+  });
+  /* Scroll Nav */
+  $(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
     });
-    /* Scroll Nav */  
-        $(function() {
-      $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 1000);
-            return false;
-          }
-        }
-      });
-    }); 
-   
-    /*MOBILE NAV*/
-   $('.js-mobile-res').click(function(){
-     var icon = $('.js-mobile-res i');
-     var nav = $('.main-nav');
-     
-     if (icon.hasClass('ion-md-menu')){
-       icon.addClass('ion-md-close');
-       icon.removeClass('ion-md-menu');
-     }else{
-       icon.addClass('ion-md-menu');
-       icon.removeClass('ion-md-close');
-     }
-     nav.toggleClass('active')  
-   });
-});
-$(function(){
+  });
+
+  /*MOBILE NAV*/
+  $('.js-mobile-res').click(function () {
+    var icon = $('.js-mobile-res i');
+    var nav = $('.main-nav');
+
+    if (icon.hasClass('ion-md-menu')) {
+      icon.addClass('ion-md-close');
+      icon.removeClass('ion-md-menu');
+    } else {
+      icon.addClass('ion-md-menu');
+      icon.removeClass('ion-md-close');
+    }
+    nav.toggleClass('active')
+  });
+$(function () {
   new WOW().init();
 });
